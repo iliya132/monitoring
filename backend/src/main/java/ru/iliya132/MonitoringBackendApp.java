0d4986afe20443668e94be958a4fd413375dde09
Ilya <iliya132@live.com>
2023-07-19 21:14:29 +0300

    initialized empty gradle project

---
+++ backend/src/main/java/ru/iliya132/MonitoringBackendApp.java
@@ -0,0 +1,11 @@
+package ru.iliya132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringBackendApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringBackendApp.class, args);
+    }
+}
---
+++ backend/src/main/java/ru/iliya132/config/AppConfig.java
@@ -0,0 +1,7 @@
+package ru.iliya132.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
