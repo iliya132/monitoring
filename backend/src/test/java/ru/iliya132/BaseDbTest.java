b69eb2e7b8811e6f0d77357e5caa086b1061976e
Ilya <iliya132@live.com>
2023-07-23 19:42:36 +0300

    auth added tests

--- backend/src/test/java/ru/iliya132/BaseDbTest.java
+++ backend/src/test/java/ru/iliya132/BaseDbTest.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.iliya132.config.TestDbConfig;
 
+// Requires docker to run
 @SpringBootTest(classes = {TestDbConfig.class, MonitoringBackendApp.class})
 @ActiveProfiles("test")
 public class BaseDbTest {
