e926800a8ef40ad7f855a30c55c56b2ec3387a98
Ilya <iliya132@live.com>
2023-07-26 19:59:10 +0300

    added spring boot test

--- backend/src/test/java/ru/iliya132/security/AuthTest.java
+++ backend/src/test/java/ru/iliya132/security/AuthTest.java
@@ -7,6 +7,7 @@ import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.message.Bas
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -16,6 +17,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import ru.iliya132.BaseMvcTest;
+import ru.iliya132.MonitoringBackendApp;
+import ru.iliya132.config.TestDbConfig;
 import ru.iliya132.model.User;
 import ru.iliya132.service.security.UserDetailsServiceImpl;
 
@@ -26,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
+@SpringBootTest(classes = {TestDbConfig.class, MonitoringBackendApp.class})
 public class AuthTest extends BaseMvcTest {
     @Autowired
     private WebApplicationContext wac;
